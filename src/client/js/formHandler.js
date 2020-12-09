c4dc0e384bb4aaa2f700c4bbf7b979e50f24e68b
omama-glitch <omamatalal@omama.local>
2020-12-09 08:25:37 +0300

    add workbox to webpack.dev.j

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,25 +6,26 @@ function handleSubmit(event) {
     
     if ( Client.checkUrl(formText) === true){
         console.log("::: Form Submitted :::")
-    }
-    fetch('http://localhost:8081/nlp', {
-        method: 'POST', 
-        credentials: 'same-origin',
-        headers: { 'Content-Type': 'application/json'},
-          // convert body data type to string      
-        body: JSON.stringify({url: formText}), 
-    });
     
-    let req = fetch('http://localhost:8081/data');
-    try{
-        req.json();
-        console.log("form handler::", req);
-        document.getElementById('agreement').innerHTML = 'Agreement: '+req.agreement;
-        document.getElementById('subjectivity').innerHTML = 'Subjectivity: '+req.subjectivity;
-        document.getElementById('confidence').innerHTML = 'Confidence: '+req.confidence;
-        document.getElementById('irony').innerHTML = 'Irony: ' +req.irony; 
-    }catch(error){
-        console.log(error);
+        fetch('http://localhost:8081/nlp', {
+            method: 'POST', 
+            credentials: 'same-origin',
+            headers: { 'Content-Type': 'application/json'},
+          // convert body data type to string      
+            body: JSON.stringify({url: formText}), 
+        });
+    
+        let req = fetch('http://localhost:8081/data');
+        try{
+            req.json();
+            console.log("form handler::", req);
+            document.getElementById('agreement').innerHTML = 'Agreement: '+req.agreement;
+            document.getElementById('subjectivity').innerHTML = 'Subjectivity: '+req.subjectivity;
+            document.getElementById('confidence').innerHTML = 'Confidence: '+req.confidence;
+            document.getElementById('irony').innerHTML = 'Irony: ' +req.irony; 
+        }catch(error){
+            console.log(error);
+        }
     }
    
    
